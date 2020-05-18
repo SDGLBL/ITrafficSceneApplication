f9cb0a4ea1f9422e869854d1936e6c1a62dcf6b2
SDGLBL <lijieisguge@gmail.com>
2020-05-18 22:19:20 +0800

    删除img_info.json

--- components/version.py
+++ components/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Mon May 18 18:09:49 2020
-__version__ = '1.0.0+fe1c910'
+# TIME: Mon May 18 21:31:20 2020
+__version__ = '1.0.0+498949a'
 short_version = '1.0.0'
 version_info = (1, 0, 0)
