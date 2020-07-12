24b43e1e0b6943b7d797685f66b384831360225d
SDGLBL <lijieisguge@gmail.com>
2020-07-12 15:07:36 +0800

    :bug: 修复自动删除违规图像的bug

--- utils/sqlitedao.py
+++ utils/sqlitedao.py
@@ -52,12 +52,12 @@ def create_database(clear_exist = False):
         clear_exist (bool, optional): 是否先清除已经存在的db数据库. Defaults to False.
     """
     if os.path.exists(DataConfig.DATABASE_PATH) and clear_exist:
+        # 清理违规图像
         os.remove(DataConfig.DATABASE_PATH)
-    # 清理违规图像
-    for name in os.listdir(DataConfig.CRIMINAL_DIR):
-        if name == 'upload':
-            continue
-        os.remove(os.path.join(DataConfig.CRIMINAL_DIR,name))
+        for name in os.listdir(DataConfig.CRIMINAL_DIR):
+            if name == 'upload':
+                continue
+            os.remove(os.path.join(DataConfig.CRIMINAL_DIR,name))
     create_traffic_str = "CREATE TABLE IF NOT EXISTS traffic (start_time_id TEXT NOT NULL,start_time INTEGER NOT NULL,end_time INTEGER NOT NULL,passage_type TEXT DEFAULT straight,obj_type TEXT DEFAULT car,number_plate TEXT,other_info TEXT,CONSTRAINT traffic_PK PRIMARY KEY (start_time_id));"
     create_criminal_str = "CREATE TABLE IF NOT EXISTS criminal (start_time_id TEXT NOT NULL,number_plate TEXT,img_path TEXT,criminal_type TEXT NOT NULL,CONSTRAINT criminal_PK PRIMARY KEY (start_time_id),CONSTRAINT criminal_FK FOREIGN KEY (start_time_id) REFERENCES traffic(start_time_id));"
     conn = get_connection(DataConfig.DATABASE_PATH)
