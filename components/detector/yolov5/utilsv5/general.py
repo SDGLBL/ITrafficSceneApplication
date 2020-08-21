47d17013b86816644560dcfda7845564f4af0d43
SDGLBL <lijieisguge@gmail.com>
2020-08-21 11:19:32 +0800

    :sparkles: add yolov5

--- components/detector/yolov5/utilsv5/general.py
+++ components/detector/yolov5/utilsv5/general.py
@@ -983,7 +983,7 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=None):
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
     if label:
-        tf = max(tl - 1, 1)  # font thickness
+        tf = max(tl - 3, 1)  # font thickness
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
         cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
