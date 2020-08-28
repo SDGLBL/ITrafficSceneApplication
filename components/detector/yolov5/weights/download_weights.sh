2c7d9b759b04b398a1ca675a35317e50d6da9b5f
SDGLBL <lijieisguge@gmail.com>
2020-08-28 12:09:58 +0800

    :bug: 修复部署bug以及更新部署文档

--- components/detector/yolov5/weights/download_weights.sh
+++ components/detector/yolov5/weights/download_weights.sh
@@ -2,6 +2,8 @@
 # Download common models
 
 python -c "
+import matplotlib
+matplotlib.use('Agg')
 from components.detector.yolov5.utilsv5.google_utils import *;
 attempt_download('components/detector/yolov5/weights/yolov5s.pt');
 attempt_download('components/detector/yolov5/weights/yolov5m.pt');
